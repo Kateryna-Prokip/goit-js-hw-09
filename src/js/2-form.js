e87d34932d1e56da85829888971d7ef8cff21e22
Kateryna-Prokip <153538553+Kateryna-Prokip@users.noreply.github.com>
2024-05-13 18:35:32 +0300

    hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,39 +1,40 @@
-const formRefs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('input[name="email"]'),
-    message: document.querySelector('textarea[name="message"]'),
-  };
-  
-  const formData = { email: '', message: '' };
-  
-  const FEEDBACK_FORM_KEY = 'feedback-form-state';
-  
-  formRefs.form.addEventListener('input', event => {
-    const { name, value } = event.target;
-  
-    formData.email = formRefs.email.value;
-    formData.message = formRefs.message.value;
-    localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(formData));
-  });
-  
-  if (localStorage.getItem(FEEDBACK_FORM_KEY)) {
-    const data = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
-    formRefs.email.value = data.email || '';
-    formRefs.message.value = data.message || '';
+const feedbackForm = document.querySelector('form.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+let localStorageData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
+const setFormValuesFromLocalStorage = () => {
+  email.value = localStorageData.email ? localStorageData.email.trim() : '';
+  message.value = localStorageData.message ? localStorageData.message.trim() : '';
+};
+
+const handleInput = (event, field) => {
+  const value = event.target.value.trim();
+  localStorageData = { ...localStorageData, [field]: value };
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const form = event.target;
+
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    alert('The email and message fields must be filled in');
+    return;
   }
-  
-  formRefs.form.addEventListener('submit', event => {
-    event.preventDefault();
-    if (
-      formRefs.email.value.trim() === '' ||
-      formRefs.message.value.trim() === ''
-    ) {
-      alert('Please, fill in all the fields');
-    } else {
-      localStorage.removeItem(FEEDBACK_FORM_KEY);
-      formRefs.form.reset();
-      console.log(formData);
-      formData.email = '';
-      formData.message = '';
-    }
-  }); 
+
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+  localStorageData = {};
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
+  form.reset();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  setFormValuesFromLocalStorage(); // Встановлення значень форми при завантаженні сторінки
+});
+
+email.addEventListener('input', event => handleInput(event, 'email'));
+message.addEventListener('input', event => handleInput(event, 'message'));
+feedbackForm.addEventListener('submit', handleSubmit);
